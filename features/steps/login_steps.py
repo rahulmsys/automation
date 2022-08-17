b079d380f91d72eac1bbe0f712e6e6c9b0e7145f
rahul.raj <rraj@msystechnologies.com>
2022-08-17 18:18:14 +0530

    updated selenium methods in steps

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -7,6 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from locators.login_page_locators import *
 from locators.home_page_locators import *
 from config.conf_vars import *
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @given('Open web browser')
@@ -19,27 +20,26 @@ def open_web_browser(context):
 
 @when('I am on the OrangeHRM login page')
 def login_page(context):
-    assert context.driver.title == login_page_title
+    assert context.driver.wait.until(EC.title_is(login_page_title)) is True
 
 
 @then('I enter "{uname}" as username and "{pwd}" as password')
 def enter_username_and_password(context, uname, pwd):
-    context.driver.wait.until(lambda driver: driver.find_element(By.XPATH, LoginPageLocators.USERNAME_FIELD)).send_keys(
+    context.driver.wait.until(EC.presence_of_element_located((By.XPATH, LoginPageLocators.USERNAME_FIELD))).send_keys(
         uname)
-    context.driver.wait.until(lambda driver: driver.find_element(By.XPATH, LoginPageLocators.PASSWORD_FIELD)).send_keys(
+    context.driver.wait.until(EC.presence_of_element_located((By.XPATH, LoginPageLocators.PASSWORD_FIELD))).send_keys(
         pwd)
 
 
 @then('I click on login button')
 def click_on_login_button(context):
-    context.driver.wait.until(lambda driver: driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BTN)).click()
+    context.driver.wait.until(EC.element_to_be_clickable((By.XPATH, LoginPageLocators.LOGIN_BTN))).click()
 
 
 @then('I should see the OrangeHRM home page')
 def verify_home_page(context):
     try:
-        assert context.driver.wait.until(
-            lambda driver: driver.find_element(By.XPATH, HomePageLocators.DASHBOARD_LOGO)).is_displayed() is True
+        context.driver.wait.until(EC.presence_of_element_located((By.XPATH, HomePageLocators.DASHBOARD_LOGO))) is True
     except Exception as e:
         print(e)
         context.driver.quit()
